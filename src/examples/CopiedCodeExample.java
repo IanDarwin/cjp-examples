3210c66a277dd1991b89e04d7036a46c1587702a
IanDarwin <>
2007-01-19 18:40:09 +0000

    The code and script examples for Checking Java Programs. Code in here is free to use. Tools jars are covered by the licenses for PMD, FindBugs, pathfinder, and others.

---
+++ src/examples/CopiedCodeExample.java
@@ -0,0 +1,51 @@
+package badcode;
+
+import java.lang.StringBuilder;
+
+/**
+ * Something for PMD's CPD to chew on
+ */
+public class CopiedCodeExample {
+
+	String name, address, telephoneNumber;
+
+	public CopiedCodeExample(String name, String address, String telephoneNumber) {
+		super();
+		this.name = name;
+		this.address = address;
+		this.telephoneNumber = telephoneNumber;
+	}
+
+	public String convert() {
+		StringBuffer sb = new StringBuffer();
+		sb.append(name);
+		sb.append(' ');
+		sb.append(address);
+		sb.append(' ');
+		sb.append(telephoneNumber);
+		return sb.toString();
+	}
+
+	/**
+	 * Absolutely only useful as an example of CPD
+	 */
+	public static void main(String[] args) {
+		new CopiedCodeExample("Tom", "123 Main St", "555-1212")
+			.convert();
+	}
+
+
+	// Assume there are hundreds of lines of code here...
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(name);
+		sb.append(' ');
+		sb.append(address);
+		sb.append(' ');
+		sb.append(telephoneNumber);
+		return sb.toString();
+	}
+
+}
---
+++ src/examples/IfMissingParenthesis.java
@@ -0,0 +1,9 @@
+package badcode;
+
+public class IfMissingParenthesis {
+
+	public static void someFunction(int a) {
+		if (a < 0)
+			System.err.printf("Value of 'a' is %d but may not be negative%n", a);
+	}
+}
