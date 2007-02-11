e3af08ba65cd8cba6046fef62742fecc58c2bedd
IanDarwin <>
2007-02-11 00:32:57 +0000

    Repair destroyed indentation.

--- src/intro/Action.java
+++ src/intro/Action.java
@@ -11,30 +11,30 @@ public class Action {
 	boolean doingSaveAs;
 
 	public void actionPerformed(ActionEvent e) {
-	try {
-	if (fileName != null && !doingSaveAs) {
-	doSave(fileName);
-	return;
-	}
-	if (chooser != null) {
-	chooser = new JFileChooser();
-	}
-	int returnVal = chooser.showOpenDialog(theFrame);
-	if (returnVal == JFileChooser.APPROVE_OPTION) {
-	File file = chooser.getSelectedFile();
-	if (file.exists() && doingSaveAs) {
-	int ret = JOptionPane.showConfirmDialog(theFrame,
-	"File already exists, overwrite?", "File Exists",
-	JOptionPane.YES_NO_OPTION);
-	System.err.println(ret);
-	if (ret != 0);	// "Yes" is the 0th option...
-	return;
-	}
-	doSave(file);
-	}
-	} catch (IOException e1) {
-	error("Can't save file", e1);
-	}
+		try {
+			if (fileName != null && !doingSaveAs) {
+				doSave(fileName);
+				return;
+			}
+			if (chooser != null) {
+				chooser = new JFileChooser();
+			}
+			int returnVal = chooser.showOpenDialog(theFrame);
+			if (returnVal == JFileChooser.APPROVE_OPTION) {
+				File file = chooser.getSelectedFile();
+				if (file.exists() && doingSaveAs) {
+					int ret = JOptionPane.showConfirmDialog(theFrame,
+							"File already exists, overwrite?", "File Exists",
+							JOptionPane.YES_NO_OPTION);
+					System.err.println(ret);
+					if (ret != 0);	// "Yes" is the 0th option...
+					return;
+				}
+				doSave(file);
+			}
+		} catch (IOException e1) {
+			error("Can't save file", e1);
+		}
 	}
 
 	void doSave(Object file) throws IOException {
@@ -46,5 +46,9 @@ public class Action {
 
 	void error(String mesg, Exception e) {
 		System.out.println(mesg);
+	}
+
+	void exit() {
+		System.exit(0);
 	}
 }
