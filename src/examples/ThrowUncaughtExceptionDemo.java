919198a95eb511c402f8a40fb3c31c256d1af6fe
IanDarwin <>
2007-02-11 20:24:25 +0000

    Finish the job.

--- src/examples/ThrowUncaughtExceptionDemo.java
+++ src/examples/ThrowUncaughtExceptionDemo.java
@@ -5,20 +5,20 @@ public class ThrowUncaughtExceptionDemo {
 	int n = 0;
 
 	public static void main(String[] args) {
-		System.out.println("ThrowUncaughtException.main() starting");
+		System.out.println("ThrowUncaughtExceptionDemo.java.main() starting");
 		new ThrowUncaughtExceptionDemo(args).process();
-		System.out.println("ThrowUncaughtException.main() done.");
+		System.out.println("ThrowUncaughtExceptionDemo.java.main() done.");
 	}
 
 	public ThrowUncaughtExceptionDemo(String[] args) {
-		System.out.println("ThrowUncaughtException.ThrowUncaughtException()");
+		System.out.println("ThrowUncaughtExceptionDemo.java.ThrowUncaughtException()");
 		if (args.length == 1) {
 			n = Integer.parseInt(args[0]);
 		}
 	}
 
 	private void process() {
-		System.out.println("ThrowUncaughtException.process()");
+		System.out.println("ThrowUncaughtExceptionDemo.java.process()");
 		if (n == 42) {
 			throw new IllegalArgumentException("That is not the meaning of this program");
 		}
