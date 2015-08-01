56859f4d882537bb185774bbd780c7a1a9a1805f
Ian Darwin <ian@darwinsys.com>
2015-08-01 16:16:45 -0700

    Comment

--- src/main/java/examples/ImpossibleToTest.java
+++ src/main/java/examples/ImpossibleToTest.java
@@ -1,7 +1,8 @@
 package examples;
 
 /** Simple example of a class that could not be tested exhaustively
- * because of the combinatoric explosion of all possible states.
+ * because of the combinatoric explosion of all possible states (there are
+ * 2^32 * 2^64 = 4^96 possible states; don't try enumerating them at home, kids...).
  */
 public class ImpossibleToTest {
 	int x;
