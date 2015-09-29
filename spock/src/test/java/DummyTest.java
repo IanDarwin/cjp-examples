99155d8bb3b21cc7f7550121ee7aa0fb06f196f6
Ian Darwin <ian@darwinsys.com>
2015-09-29 14:02:53 -0400

    Add one Java JUnit test to show that they play nice together.

---
+++ spock/src/test/java/DummyTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+/**
+ * This test is just here to show that Java/JUnit and Groovy/Spock can run
+ * together in the same project without any special setup.
+ */
+public class DummyTest {
+
+	@Test
+	public void testOne() {
+		System.out.println("DummyTest.testOne() was here");
+	}
+}
