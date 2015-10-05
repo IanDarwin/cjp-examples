6e74f8f8768ba2f60e9cab629e849664d8f0d9fa
Ian Darwin <ian@darwinsys.com>
2015-10-05 10:52:17 -0400

    Static import java.lang.Math, to agree with video script.

--- general/src/test/java/junit/DoubleWithEpsilonTest.java
+++ general/src/test/java/junit/DoubleWithEpsilonTest.java
@@ -2,12 +2,13 @@ package junit;
 
 import org.junit.*;
 
+import static java.lang.Math.sqrt;
 import static org.junit.Assert.*;
 
 public class DoubleWithEpsilonTest {
 
 	@Test
 	public void testSqrt() {
-		assertEquals("test sqrt", 1.414213562D, Math.sqrt(2), 0.00000001D);
+		assertEquals("test sqrt", 1.414213562D, sqrt(2), 0.00000001D);
 	}
 }
