fc3108584becfc140fa2f359a29a8b1e0685ca25
Ian <ian@darwinsys.com>
2016-01-25 13:27:14 -0500

    Comments repair

--- spock/src/test/groovy/StringBuilderTest.groovy
+++ spock/src/test/groovy/StringBuilderTest.groovy
@@ -3,7 +3,7 @@ import spock.lang.*
 class StringBuilderSpec extends Specification {
   def "StringBuilder append"() {
     expect:
-		// un-comment ending of following line to see a test fail (no space).
+		// un-comment ending of following line to see a test fail (because "HelloWorld" has no space).
 		new StringBuilder().append("Hello").append("World") // .equals("Hello World")
   }
 }
