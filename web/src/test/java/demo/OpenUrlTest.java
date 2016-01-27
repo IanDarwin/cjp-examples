a2963dcdfcc70dff0097f334a77ad553595bb592
Ian Darwin <ian@darwinsys.com>
2016-01-27 13:18:50 -0500

    Start of web testing project.

---
+++ web/src/test/java/demo/OpenUrlTest.java
@@ -0,0 +1,20 @@
+package demo;
+
+import java.net.URL;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class OpenUrlTest {
+
+	@Test
+	public void testServerIsUp() throws Exception {
+		new URL("http://darwinsys.com/").openConnection().getInputStream().close();;
+	}
+	
+	@Test
+	@Ignore // Uncomment to show failure
+	public void testAnotherServerIsUp() throws Exception {
+		new URL("http://nosuchhostuo2i3uoifiooaa.com/").openConnection().getInputStream().close();
+	}
+}
