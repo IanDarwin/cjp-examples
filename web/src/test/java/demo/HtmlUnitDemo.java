63c8be570b4bd02486fa85a6cf00de3859a98a65
Ian <ian@darwinsys.com>
2016-01-31 09:30:23 -0500

    Disable JavaScript since JS runs page a full order of magnitude slower.

--- web/src/test/java/demo/HtmlUnitDemo.java
+++ web/src/test/java/demo/HtmlUnitDemo.java
@@ -18,22 +18,21 @@ public class HtmlUnitDemo {
 	    	final WebClientOptions options = webClient.getOptions();
 			options.setThrowExceptionOnScriptError(false);
 			options.setThrowExceptionOnFailingStatusCode(false);
+			options.setJavaScriptEnabled(false); // bypass horrible slowdown
 		    
 	        final HtmlPage page = webClient.getPage("http://oreilly.com");
 	        assertTrue(page.getTitleText().contains("O'Reilly Media"));
 
 	        // Fill in form and click "Search" button/link
 	        HtmlInput searchBox = page.getElementByName("q");
-	        System.out.println(searchBox);
+	        // System.out.println(searchBox);
 	        searchBox.setValueAttribute("0636920026518");
-	        // The button has no ID or Name but is inside a div with this id:
+	        // The image-button we want to click has no ID or Name but is inside a div with this id:
 	        HtmlInput searchButton = (HtmlInput) page.getElementById("search-button").getChildNodes().get(0);
 	        // Clicking the button should move to a new page
-	        System.out.println(searchButton);
+	        // System.out.println(searchButton);
 		    HtmlPage page2 = searchButton.click();
 
-			System.out.println(page2.getUrl());
-			
 		    // Check for results
 	        final String pageAsText = page2.asText();
 	        // System.out.println(pageAsText);
