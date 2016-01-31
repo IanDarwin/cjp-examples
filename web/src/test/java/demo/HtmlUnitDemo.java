633eebaa8ae5340b02eb22c139707f00987cedf1
Ian <ian@darwinsys.com>
2016-01-31 09:19:52 -0500

    Fold in an HtmlUnit demo.

---
+++ web/src/test/java/demo/HtmlUnitDemo.java
@@ -0,0 +1,43 @@
+package demo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.WebClientOptions;
+import com.gargoylesoftware.htmlunit.html.HtmlForm;
+import com.gargoylesoftware.htmlunit.html.HtmlInput;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
+public class HtmlUnitDemo {
+
+	@Test
+	public void pageTest() throws Exception {
+	    try (final WebClient webClient = new WebClient()) {
+	    	final WebClientOptions options = webClient.getOptions();
+			options.setThrowExceptionOnScriptError(false);
+			options.setThrowExceptionOnFailingStatusCode(false);
+		    
+	        final HtmlPage page = webClient.getPage("http://oreilly.com");
+	        assertTrue(page.getTitleText().contains("O'Reilly Media"));
+
+	        // Fill in form and click "Search" button/link
+	        HtmlInput searchBox = page.getElementByName("q");
+	        System.out.println(searchBox);
+	        searchBox.setValueAttribute("0636920026518");
+	        // The button has no ID or Name but is inside a div with this id:
+	        HtmlInput searchButton = (HtmlInput) page.getElementById("search-button").getChildNodes().get(0);
+	        // Clicking the button should move to a new page
+	        System.out.println(searchButton);
+		    HtmlPage page2 = searchButton.click();
+
+			System.out.println(page2.getUrl());
+			
+		    // Check for results
+	        final String pageAsText = page2.asText();
+	        // System.out.println(pageAsText);
+	        assertTrue(pageAsText.contains("Java Cookbook"));
+	    }
+	}
+}
