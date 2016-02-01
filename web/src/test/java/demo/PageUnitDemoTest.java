571e1dbe95c687944a60e190384e206db86eb9f4
Ian <ian@darwinsys.com>
2016-02-01 15:48:09 -0500

    Give a PageUnit example running under JUnit...

---
+++ web/src/test/java/demo/PageUnitDemoTest.java
@@ -0,0 +1,13 @@
+package demo;
+
+import org.junit.Test;
+
+import pageunit.ScriptTestCase;
+
+public class PageUnitDemoTest {
+
+	@Test
+	public void demo() throws Exception {
+		new ScriptTestCase("src/test/pageunit/oreilly.txt").run();
+	}
+}
