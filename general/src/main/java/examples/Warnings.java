f74275f76a564df810e0c8897950259aa6947891
Ian <ian@darwinsys.com>
2016-02-06 11:15:53 -0500

    Example that gives warnings

---
+++ general/src/main/java/examples/Warnings.java
@@ -0,0 +1,16 @@
+package general;
+
+import java.util.Date;
+
+public class Warnings {
+
+	private int x;	// Expect UNUSED Warning
+
+	public static void main(String[] args) {
+		int y;
+
+        // Create a Date object for May 5, 1986
+        Date d = new Date(86, 5-1, 5);   // EXPECT DEPRECATION WARNING
+        System.out.println("Date is " + d);
+	}
+}
