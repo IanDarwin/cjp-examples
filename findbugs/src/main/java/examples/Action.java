c49598f5b289b94d3e3d63e30e4a6721ef061515
Ian <ian@darwinsys.com>
2016-02-08 09:18:05 -0500

    Duplicate the Action class from ../general here
    It's mainly for FB use but I can't be sure it wasn't referred to
    in some earlier chapter; you still can't vgrep in movies yet.

---
+++ findbugs/src/main/java/examples/Action.java
@@ -0,0 +1,58 @@
+package intro;
+
+import java.io.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+/**
+ * WARNING - this class has some deliberate errors in it.
+ * DO NOT clean up this file, or you will break the error detection examples!
+ */
+public class Action {
+	JFrame theFrame;
+	JFileChooser chooser;
+	String fileName;
+	boolean doingSaveAs;
+
+	public void actionPerformed(ActionEvent e) {
+		try {
+			if (fileName != null && !doingSaveAs) {
+				doSave(fileName);
+				return;
+			}
+			if (chooser != null) {
+				chooser = new JFileChooser();
+			}
+			int returnVal = chooser.showOpenDialog(theFrame);
+			if (returnVal == JFileChooser.APPROVE_OPTION) {
+				File file = chooser.getSelectedFile();
+				if (file.exists() && doingSaveAs) {
+					int ret = JOptionPane.showConfirmDialog(theFrame,
+							"File already exists, overwrite?", "File Exists",
+							JOptionPane.YES_NO_OPTION);
+					System.err.println(ret);
+					if (ret != 0);	// "Yes" is the 0th option...
+						return;
+				}
+				doSave(file);
+			}
+		} catch (IOException e1) {
+			error("Can't save file", e1);
+		}
+	}
+
+	void doSave(Object file) throws IOException {
+		// dummy version
+		if (file == null) {
+			throw new IOException("Null");
+		}
+	}
+
+	void error(String mesg, Exception e) {
+		System.out.println(mesg);
+	}
+
+	void exit() {
+		System.exit(0);
+	}
+}
