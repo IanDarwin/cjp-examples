138056275fa6d5fdefea86bfb71c7d643ed73d82
Ian <ian@darwinsys.com>
2016-02-08 21:36:39 -0500

    Save the tools needed for running the datamining example.

--- findbugs/mining/mining.sh
+++ findbugs/mining/mining.sh
@@ -1,8 +1,13 @@
-for f in /tmp/darwin*.jar
+for f in darwinsys*.jar
 do 
-	findbugs -textui -onlyAnalyze com.darwinsys.- -medium -xml -outputFile $f.xml $f
+	findbugs -textui \
+	-onlyAnalyze com.darwinsys.- \
+	-auxclasspath "$HOME/lib/mail.jar:$HOME/lib/servlet-api.jar:$HOME/lib/jsp-api.jar:$HOME/lib/junit.jar" \
+	-medium -xml -outputFile $f.xml $f
 done
 
-computeBugHistory -output fbmining.xml darwinsys-api-1.0.*.xml
+computeBugHistory -output fbmining.xml darwinsys-*.xml
 
 mineBugHistory -formatDates fbmining.xml | tr '\t' '|'
+
+
