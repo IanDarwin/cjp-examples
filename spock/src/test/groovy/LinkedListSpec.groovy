272d5d152fa56e7e76cf880c5d899278776a29d0
Ian <ian@darwinsys.com>
2016-03-30 09:58:47 -0400

    Add linkedlist test

---
+++ spock/src/test/groovy/LinkedListSpec.groovy
@@ -0,0 +1,14 @@
+import spock.lang.*
+import java.util.*
+
+class LinkListSpec extends Specification {
+  @Shared def target = null
+  def setup() {
+	target = new LinkedList();
+  }
+  def "Linked List add"() {
+    expect:
+		target.add("one"); // expect keyword expects boolean-like value
+		target.get(0) == "one"
+  }
+}
