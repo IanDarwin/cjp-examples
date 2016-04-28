6d428bbab20a221c99484ff3bc3cb6182b20919c
Ian <ian@darwinsys.com>
2016-04-28 14:14:19 -0400

    Bettery style.

--- spock/src/test/groovy/MyArrayListSpec.groovy
+++ spock/src/test/groovy/MyArrayListSpec.groovy
@@ -3,10 +3,11 @@ import spock.lang.*;
 class MyArrayListSpec extends Specification {
     @Shared myArrayList = new ArrayList();
     def "ArrayList clear works"() {
-        myArrayList.add("some value")
-        when:
+		given: "The user adds something to a List"
+			myArrayList.add("some value")
+        when: "We clear the list"
             myArrayList.clear()
-        then:
+        then: "There should be nothing left in the list"
             myArrayList.size() == 0
             !myArrayList    // Shorter form based on meaning of 'truth' in Groovy.
     }
