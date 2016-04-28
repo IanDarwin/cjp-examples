94f21e3439e2625405d32bd6b2c358eca4471911
Ian <ian@darwinsys.com>
2016-04-28 14:43:19 -0400

    More commentary

--- spock/src/test/groovy/DatabaseSpec.groovy
+++ spock/src/test/groovy/DatabaseSpec.groovy
@@ -8,16 +8,17 @@ class DatabaseSpec extends Specification {
 		[url:'jdbc:postgresql:javatest', user:'javatest', password:'xecret', driver:"org.postgresql.Driver"]
 	@Shared def sql = Sql.newInstance(db.url, db.user, db.password, db.driver)
 
-	def "DatabaseTest"() {
-		setup:
+	def "As Administrator, I want to be sure there's an entry for Spock in the Database"() {
+		given:
 			def found = false
+		when: "We look to see if spock has been added to the database"
 			sql.eachRow("select name from javatest order by lower(name)") { r->
 				// println r.name
 				if (r.name.equalsIgnoreCase('spock')) {
 					found = true;
 				}
 			}
-		expect:
+		then: "We should have found it in the database"
 			found
 	}
 }
