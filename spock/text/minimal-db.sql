ae816b21f8a88ae3c6d1f866fb60dc59932c8a7b
Ian <ian@darwinsys.com>
2016-04-28 14:47:01 -0400

    Another entry in the tests database

--- spock/text/minimal-db.sql
+++ spock/text/minimal-db.sql
@@ -3,4 +3,5 @@ create database javatest owner javatest;
 \c javatest
 create table javatest(id integer primary key, name varchar);
 alter table javatest owner to javatest;
-insert into javatest(id,name) values(1,'spock');
+insert into javatest(id,name) values(1,'JUnit');
+insert into javatest(id,name) values(2,'Spock');
