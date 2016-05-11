15fce7cc9e0b9f09bf35148000abcfc81588b157
Ian <ian@darwinsys.com>
2016-05-10 22:29:09 -0400

    simplify

--- spock/src/test/groovy/DatabaseSpec.groovy
+++ spock/src/test/groovy/DatabaseSpec.groovy
@@ -9,8 +9,7 @@ class DatabaseSpec extends Specification {
 	@Shared def sql = Sql.newInstance(db.url, db.user, db.password, db.driver)
 
 	def "As Administrator, I want to be sure there's an entry for Spock in the Database"() {
-		given:
-			def found = false
+		def found = false
 		when: "We look to see if spock has been added to the database"
 			sql.eachRow("select name from javatest order by lower(name)") { r->
 				// println r.name
