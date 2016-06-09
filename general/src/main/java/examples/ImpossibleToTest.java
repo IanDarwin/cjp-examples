b9e3f2a50d41f8985b0d85a9fd0fc1df057d9aec
David Eisner <deisner@gmail.com>
2016-06-09 11:42:53 -0400

    Fix math error in comment.

--- general/src/main/java/examples/ImpossibleToTest.java
+++ general/src/main/java/examples/ImpossibleToTest.java
@@ -2,7 +2,7 @@ package examples;
 
 /** Simple example of a class that could not be tested exhaustively
  * because of the combinatoric explosion of all possible states (there are
- * 2^32 * 2^64 = 4^96 possible states; don't try enumerating them at home, kids...).
+ * 2^32 * 2^64 = 2^96 possible states; don't try enumerating them at home, kids...).
  */
 public class ImpossibleToTest {
 	int x;
