652b83ee10b85db73edd3c231d455794e4bb991a
Ian Darwin <ian@darwinsys.com>
2021-02-03 21:11:10 -0500

    Change Spock database test to use h2, instead of...
    ... depending on a database that only existed on my machine.

--- spock/src/test/groovy/DatabaseSpec.groovy
+++ spock/src/test/groovy/DatabaseSpec.groovy
@@ -1,14 +1,25 @@
 import groovy.sql.Sql
 import spock.lang.*
 
-// Will only work if database is set up, see README
-
 class DatabaseSpec extends Specification {
 	@Shared def db = 
-		[url:'jdbc:postgresql:javatest', user:'javatest', password:'xecret', driver:"org.postgresql.Driver"]
+		// For H2
+		[url:'jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE', 
+			user:'sa', password:'xecret', driver:"org.h2.Driver"]
+		// For PostgreSQL
+		// [url:'jdbc:postgresql:javatest', 
+		//	user:'javatest', password:'xecret', driver:"org.postgresql.Driver"]
+
 	@Shared def sql = Sql.newInstance(db.url, db.user, db.password, db.driver)
 
+	def setupData() {
+		sql.execute("create table if not exists javatest(id integer primary key, name varchar)");
+		sql.execute("delete from javatest");
+		sql.execute("insert into javatest(id, name) values(42, 'spock')");
+	}
+
 	def "As Administrator, I want to be sure there's an entry for Spock in the Database"() {
+		setupData();	// fakin' it: set up for this test.
 		def found = false
 		when: "We look to see if spock has been added to the database"
 			sql.eachRow("select name from javatest order by lower(name)") { r->
