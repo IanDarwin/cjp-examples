7eabcac3c2d0072559e0b95f8c88891ab9a00240
Ian Darwin <ian@darwinsys.com>
2021-02-03 20:11:17 -0500

    Comments-only change

--- spock/src/test/groovy/FailureDemo.groovy
+++ spock/src/test/groovy/FailureDemo.groovy
@@ -6,6 +6,7 @@ class FailureSpec extends Specification {
 	def "Failsome StringBuilder append"() {
 		expect:
 			// Un-comment the following line line out to see the nice failure message
+			// (comparing a StringBuilder to a string, user "forgot" to call toString()).
 			// new StringBuilder().append("Hello").append("World").equals("Hello World");
 			true
 	}
